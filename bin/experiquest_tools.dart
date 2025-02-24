60dc92783057e736330caf3b2151267b2e4f5eed
John Angelo Cordero <johnangelocordero.dev@gmail.com>
2025-02-24 13:27:27 +0800

    Update experiquest_tools.dart

--- bin/experiquest_tools.dart
+++ bin/experiquest_tools.dart
@@ -11,10 +11,10 @@ C) The range and depth at which the research will be explored
 D) The variables and their characteristics being studied
 
 2. How do delimitations in a study differ from the scope?
-A) Delimitations focus on the range of data interpretation, while scope focuses on the variables.
-B) Delimitations describe the boundaries and constraints the researcher sets, while the scope defines the range of the study.
-C) Delimitations refer to the research methods used, while the scope defines the research questions.
-D) Delimitations are irrelevant in the context of research design and methodology.
+A) Delimitations focus on the range of data interpretation, while scope focuses on the variables
+B) Delimitations describe the boundaries and constraints the researcher sets, while the scope defines the range of the study
+C) Delimitations refer to the research methods used, while the scope defines the research questions
+D) Delimitations are irrelevant in the context of research design and methodology
 
 3. What is an example of a delimitation in a study on bullying among teenagers?
 A) Including all age groups in the study
@@ -35,7 +35,7 @@ C) The personal opinions of the researcher
 D) The participants' age, sex, or traits
 
 
-'''
+''',
 ];
 
 void main() {
@@ -53,18 +53,61 @@ void main() {
       level: $index,
       levelName: Module $module Chapter $chapter Lesson $lesson,
       monsterName: '???',
-      monsterFilename: 'assets/images/monsters/lvl${index % 3}_monster.png'
       bg: 'assets/images/backgrounds/lvl${index % 3}_bg.png',
       lesson: ref.read(reviewersProvider)[${module - 1}].modules[${chapter - 1}].lesson[${lesson - 1}],
-      Question(
-        question: "${getSentenceAfterString(data: data, string: '1. ')}",
-        choices: {
-          Choice.A: "${getSentenceAfterString(data: data, string: 'A)')}"
-          Choice.B: "${getSentenceAfterString(data: data, string: 'B)')}"
-          Choice.C: "${getSentenceAfterString(data: data, string: 'C)')}"
-          Choice.D: "${getSentenceAfterString(data: data, string: 'D)')}"
-        },
-      ),
+      
+      questions: [ 
+        Question(
+          question: "${getSentenceAfterString(data: data, string: '1.', delimiter: '?')}",
+          answer: ,
+          choices: {
+            Choice.A: "${getSentenceAfterString(data: data, string: 'A)', startingString: '1.')}",
+            Choice.B: "${getSentenceAfterString(data: data, string: 'B)', startingString: '1.')}",
+            Choice.C: "${getSentenceAfterString(data: data, string: 'C)', startingString: '1.')}",
+            Choice.D: "${getSentenceAfterString(data: data, string: 'D)', startingString: '1.')}",
+          },
+        ),
+        Question(
+          question: "${getSentenceAfterString(data: data, string: '2.', delimiter: '?')}",
+          answer: ,
+          choices: {
+            Choice.A: "${getSentenceAfterString(data: data, string: 'A)', startingString: '2.')}",
+            Choice.B: "${getSentenceAfterString(data: data, string: 'B)', startingString: '2.')}",
+            Choice.C: "${getSentenceAfterString(data: data, string: 'C)', startingString: '2.')}",
+            Choice.D: "${getSentenceAfterString(data: data, string: 'D)', startingString: '2.')}",
+          },
+        ),
+        Question(
+          question: "${getSentenceAfterString(data: data, string: '3.', delimiter: '?')}",
+          answer: ,
+          choices: {
+            Choice.A: "${getSentenceAfterString(data: data, string: 'A)', startingString: '3.')}",
+            Choice.B: "${getSentenceAfterString(data: data, string: 'B)', startingString: '3.')}",
+            Choice.C: "${getSentenceAfterString(data: data, string: 'C)', startingString: '3.')}",
+            Choice.D: "${getSentenceAfterString(data: data, string: 'D)', startingString: '3.')}",
+          },
+        ),
+        Question(
+          question: "${getSentenceAfterString(data: data, string: '4.', delimiter: '?')}",
+          answer: ,
+          choices: {
+            Choice.A: "${getSentenceAfterString(data: data, string: 'A)', startingString: '4.')}",
+            Choice.B: "${getSentenceAfterString(data: data, string: 'B)', startingString: '4.')}",
+            Choice.C: "${getSentenceAfterString(data: data, string: 'C)', startingString: '4.')}",
+            Choice.D: "${getSentenceAfterString(data: data, string: 'D)', startingString: '4.')}",
+          },
+        ),
+        Question(
+          question: "${getSentenceAfterString(data: data, string: '5.', delimiter: '?')}",
+          answer: ,
+          choices: {
+            Choice.A: "${getSentenceAfterString(data: data, string: 'A)', startingString: '5.')}",
+            Choice.B: "${getSentenceAfterString(data: data, string: 'B)', startingString: '5.')}",
+            Choice.C: "${getSentenceAfterString(data: data, string: 'C)', startingString: '5.')}",
+            Choice.D: "${getSentenceAfterString(data: data, string: 'D)', startingString: '5.')}",
+          },
+        ),
+      ]
     ),
     ''';
 
@@ -73,31 +116,32 @@ void main() {
 }
 
 int getNumberAfterString({required String data, required String string}) {
-  // Use a regular expression to find "Module" followed by a number.
   final regex = RegExp('$string (\\d+)');
   final match = regex.firstMatch(data);
-
-  // If a match is found, parse the number and return it.
-  if (match != null) {
-    return int.parse(match.group(1)!);
-  }
-
-  throw Exception('no match for $string');
+  return int.parse(match!.group(1)!);
 }
 
-String? getSentenceAfterString({required String data, required String string}) {
-  // Create a regular expression to match the target string followed by a newline, period, or question mark,
-  // then capture the sentence after it.
-  final regex = RegExp(
-    r'(?<=\b' + RegExp.escape(string) + r')[\.\n\?]\s*(.*?)(?=[\.\n\?]|\s|$)',
-    multiLine: true,
-  );
+String getSentenceAfterString({
+  required String data,
+  required String string,
+  String? startingString,
+  String? delimiter,
+}) {
+  String substring = '';
 
-  final match = regex.firstMatch(data);
-
-  if (match != null) {
-    return match.group(1);
+  if (startingString != null) {
+    String tmp = data.substring(data.indexOf(startingString));
+    substring = tmp.substring(tmp.indexOf(string));
+  } else {
+    substring = data.substring(data.indexOf(string));
   }
 
-  throw Exception('No match for $string');
+  int indexOfDelimiter =
+      delimiter != null
+          ? substring.indexOf(delimiter)
+          : substring.indexOf('\n');
+
+  String question = substring.substring(2, indexOfDelimiter + 1).trim();
+
+  return question;
 }
